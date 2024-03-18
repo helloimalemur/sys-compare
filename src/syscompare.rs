dcc2c70dcd8cef5a2a977038d967ead514da4362
jamesk <james.k@konnektive.com>
2024-03-18 17:42:04 -0400

    init

--- src/syscompare.rs
+++ src/syscompare.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 use Fasching::snapshot::Snapshot;
 
 pub enum CompareMode {
@@ -6,13 +8,14 @@ pub enum CompareMode {
 
 pub struct SysCompareApp {
     mode: CompareMode,
-    args: Vec<String>
+    args: Vec<String>,
+    comparatives: Arc<Mutex<HashMap<String, Snapshot>>>
 }
 
 
 impl SysCompareApp {
     pub fn new(mode: CompareMode, args: Vec<String>) -> SysCompareApp {
-        SysCompareApp { mode, args }
+        SysCompareApp { mode, args, comparatives: Arc::new(Mutex::new(HashMap::new())) }
     }
     pub fn run(&self) {
         println!("running");
@@ -21,7 +24,7 @@ impl SysCompareApp {
 
 impl Default for SysCompareApp {
     fn default() -> Self {
-        SysCompareApp { mode: CompareMode::Local2Local, args: vec![] }
+        SysCompareApp { mode: CompareMode::Local2Local, args: vec![], comparatives: Arc::new(Mutex::new(HashMap::new())) }
     }
 }
 

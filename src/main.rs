06ee41539473af5eb3730727e37b8459661998e9
jamesk <james.k@konnektive.com>
2024-03-29 16:38:58 -0400

    add_return_count_only_flag_on_compare

--- src/main.rs
+++ src/main.rs
@@ -18,9 +18,9 @@ fn main() {
             println!("Creating snapshot..");
             create.run()
         },
-        Commands::Compare { left, right, selection } => {
+        Commands::Compare { left, right, selection, count_only } => {
             println!("Running snapshot comparison..");
-            let mut compare = CompareMode::new(movable.clone(), left, right, selection);
+            let mut compare = CompareMode::new(movable.clone(), left, right, selection, count_only);
             compare.run()
         }
     };
